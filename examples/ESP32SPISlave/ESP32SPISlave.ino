cfb673e34ec9d92d659f7d5653e7d6cccc8c5605
AJ Keller <pushtheworldllc@gmail.com>
2018-04-09 09:44:42 -0400

    FIX: spi

--- examples/ESP32SPISlave/ESP32SPISlave.ino
+++ examples/ESP32SPISlave/ESP32SPISlave.ino
@@ -20,8 +20,20 @@
 #define LED_NOTIFY_ESP32 21
 #define VOLTAGE_SENSE_ESP32 35
 
+
 SlaveSPI slave;
-void test();
+String txt = "";
+String cmd ="";
+boolean statusNeedsToBeSent = false;
+boolean streamStart = false;
+boolean wasPolled = false;
+
+void test() {
+  Serial.println("SPI Slave Data sent");
+  txt = "";
+  // Serial.println(slave[0]);
+}
+
 String perfectPrintByteHex(uint8_t b) {
   if (b <= 0x0F) {
     return "0" + String(b, HEX);
@@ -33,31 +45,32 @@ void setup() {
   Serial.begin(115200);
   // put your setup code here, to run once:
   slave.begin((gpio_num_t)SO,(gpio_num_t)SI,(gpio_num_t)SCLK,(gpio_num_t)SS,32,test);//seems to work with groups of 4 bytes
+  wifi.begin();
 }
-String txt = "";
-String cmd ="";
-boolean statusNeedsToBeSent = false;
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(slave.getBuff()->length()&&digitalRead(SS)==HIGH) {
     while(slave.getBuff()->length())
       txt+=slave.read();
-    Serial.println("slave input:");
+    // Serial.println("slave input:");
     if (txt[0] == 0x04) {
       Serial.println("Status!");
       statusNeedsToBeSent = true;
+    } else if (txt[0] == 0x03) {
+      wasPolled = true;
     } else {
       Serial.println("Something else");
     }
-    for(int i=0;i<txt.length();i++)
-      Serial.print(perfectPrintByteHex(txt[i]));
-    Serial.println();
+    // for(int i=0;i<txt.length();i++)
+      // Serial.print(perfectPrintByteHex(txt[i]));
+    // Serial.println();
   }
   while(Serial.available()) {
     cmd +=(char) Serial.read();
   }
-  while(txt.length()>0) {
+  if(txt.length()>0) {
     if (statusNeedsToBeSent) {
       Serial.println("Fixing status");
       statusNeedsToBeSent = false;
@@ -66,18 +79,38 @@ void loop() {
       txt[2]=0;
       txt[3]=0;
       txt[4]=0;
-      txt[5]=214;
-      txt[6]=215;
-      txt[30]=209;
-      txt[31]=209;
+      slave.trans_queue(txt);
     }
-    slave.trans_queue(txt);
-    Serial.println("slave output:");
-    for(int i=0;i<txt.length();i++)
-      Serial.print(perfectPrintByteHex(txt[i]));
-    Serial.println();
+    if (wasPolled) {
+      wasPolled = false;
+      if (!streamStart) {
+        Serial.println("Starting stream");
+        // streamStart = true;
+        wifi.passthroughCommands("V");
+        slave.trans_queue(wifi.passthroughBuffer, 32);
+        // txt[0] = 1;
+        // txt[1] = 'b';
+        
+      } else {
+        // txt = "";
+        // wifi.passthroughBufferClear();
+        // slave.trans_queue(wifi.passthroughBuffer, 32);
+      }
+    }
+    if (wifi.passthroughPosition > 0) {
+      Serial.println("Pass through slave output:");
+      for(int i=0;i<32;i++)
+        Serial.print(perfectPrintByteHex(wifi.passthroughBuffer[i]));
+      Serial.println();
+    } else if (txt.length() > 0) {
+      Serial.println("Txt slave output:");
+      for(int i=0;i<txt.length();i++)
+        Serial.print(perfectPrintByteHex(txt[i]));
+      Serial.println();
+    } 
     // Serial.println(txt);
     txt ="";
+    wifi.passthroughBufferClear();
   }
   while(cmd.length()>0) {
     Serial.println("input:");
@@ -88,11 +121,3 @@ void loop() {
     cmd ="";
   }
 }
-
-void test() {
-  //Serial.println("test");
-  //Serial.println(slave[0]);
-}
-
-
-
